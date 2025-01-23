f00ea4ec651ffcf501e0687490a656ce2450b228
이지은 <tori@ryanlab.kr>
2025-01-23 15:50:16 +0900

    [feat] 응답 코드 세분화, 예외 처리 로직 추가, 메모 전체 조회 기능 구현, 메모 제목 수정 기능 추가

--- src/main/java/com/example/memo/controller/MemoController.java
+++ src/main/java/com/example/memo/controller/MemoController.java
@@ -3,11 +3,11 @@ package com.example.memo.controller;
 import com.example.memo.dto.MemoRequestDto;
 import com.example.memo.dto.MemoResponseDto;
 import com.example.memo.entity.Memo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/memos")
@@ -17,7 +17,7 @@ public class MemoController {
 
     // 메모 생성
     @PostMapping
-    public MemoResponseDto createMemo(@RequestBody MemoRequestDto dto){
+    public ResponseEntity<MemoResponseDto> createMemo(@RequestBody MemoRequestDto dto){
 
         // 식별자가 1씩 증가하도록 만듦
         Long memoId = memoList.isEmpty() ? 1 : Collections.max(memoList.keySet()) + 1;
@@ -28,36 +28,82 @@ public class MemoController {
         // Inmemory DB 에 Memo 저장
         memoList.put(memoId, memo);
 
-        return new MemoResponseDto(memo);
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.CREATED);
     }
 
-    // 메모 조회
+    // 메모 단건 조회
     @GetMapping("/{id}")
-    public MemoResponseDto findMemoById(@PathVariable Long id){
+    public ResponseEntity<MemoResponseDto> findMemoById(@PathVariable Long id){
 
         Memo memo = memoList.get(id);
 
-        return new MemoResponseDto(memo);
+        if(memo == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
+    }
+
+    // 메모 전체 조회
+    @GetMapping
+    public ResponseEntity<List<MemoResponseDto>> findAllMemos(){
+
+        // init List
+        List<MemoResponseDto> responseList = new ArrayList<>();
+
+        // HashMap<Memo> => List<MemoResponseDto>
+        for(Memo memo : memoList.values()){
+            MemoResponseDto responseDto = new MemoResponseDto(memo);
+            responseList.add(responseDto);
+        }
+
+        return new ResponseEntity<>(responseList, HttpStatus.OK);
     }
 
     // 메모 단건 전체 수정
     @PutMapping("/{id}")
-    public MemoResponseDto updateMemoById(
+    public ResponseEntity<MemoResponseDto> updateMemoById(
             @PathVariable Long id,
             @RequestBody MemoRequestDto dto
     ){
         Memo memo = memoList.get(id);
 
+        if(memo == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        if(dto.getTitle() == null || dto.getContents() == null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
         memo.update(dto);
 
-        return new MemoResponseDto(memo);
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
+    }
+
+    // 메모 단건 제목 수정
+    @PatchMapping("/{id}")
+    public ResponseEntity<MemoResponseDto> updateTitle(
+            @PathVariable Long id,
+            @RequestBody MemoRequestDto dto
+    ){
+        Memo memo = memoList.get(id);
+
+        // NPE 방지
+        if(memo == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        if(dto.getTitle() == null || dto.getContents() != null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        memo.updateTitle(dto);
+
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
     }
 
     // 메모 삭제
     @DeleteMapping("/{id}")
-    public void deleteMemo(@PathVariable Long id){
+    public ResponseEntity<Void> deleteMemo(@PathVariable Long id){
 
-        memoList.remove(id);
+        // memoList 의 key 값에 id 를 포함하고 있다면 삭제
+        if(memoList.containsKey(id)) {
+            memoList.remove(id);
 
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/example/memo/entity/Memo.java
+++ src/main/java/com/example/memo/entity/Memo.java
@@ -17,4 +17,7 @@ public class Memo {
         this.contents = dto.getContents();
     }
 
+    public void updateTitle(MemoRequestDto dto){
+        this.title = dto.getTitle();
+    }
 }
